9156dbf22c491982b4c5c0d7df63d93c8664442c
NidhitheCoder <nidhpv@gmail.com>
2020-11-30 18:45:31 +0530

    hooks implemented

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import zomato from "../api/zomato";
+import { Object } from "core-js";
+
+
+export default () => {
+  const [results, setResults] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const SearchApi = async searchTerm => {
+      console.log("haiii  here")
+    try {
+      const response = await zomato.get("/search", {
+        params: {
+          q: searchTerm
+        }
+      });
+      setResults(Object.values(response.data.restaurants));
+      setErrorMsg("");
+    } catch {
+      setErrorMsg("Something went wrong");
+    }
+  };
+
+  // SearchApi('rice'); // this is bad code and it couses infinit loop
+  useEffect(() => {
+    SearchApi("rice");
+  }, []);
+
+  return [SearchApi, results, errorMsg];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,31 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMsg, setErrMsg] = useState("");
-
-  const SearchApi = async searchTerm => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          q: searchTerm
-        }
-      });
-      setResults(Object.values(response.data.restaurants));
-      setErrMsg("");
-    } catch (err) {
-      setErrMsg("Something went wrong");
-    }
-  };
-
-  // SearchApi('rice'); // this is bad code and it couses infinit loop
-  useEffect(() => {
-    SearchApi("rice");
-  }, []);
+  const [SearchApi, results, errorMsg] = useResults();
 
   return (
     <View>
